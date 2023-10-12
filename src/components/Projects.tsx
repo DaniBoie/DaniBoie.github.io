551f16b4a00a11a72328c7164f8e1a45c14a754c
Daniel Ayala <dannyboy3385@gmail.com>
2023-10-12 07:10:32 -0700

    added other projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,9 +13,19 @@ export const Projects = () => {
         <Link className="linkStyle" to='/projects'>
           <ProjectCard url={'url(https://raw.githubusercontent.com/DaniBoie/TS-React-Portfolio/main/src/resources/SecretSauceLogo.png)'}/>
         </Link>
-        <ProjectCard url={'url(https://raw.githubusercontent.com/DaniBoie/TS-React-Portfolio/main/src/resources/OC-Hillel-logo.png-OC%2BHillel%2Blogo.png)'}/>
-        <ProjectCard url={'url(https://raw.githubusercontent.com/DaniBoie/TS-React-Portfolio/main/src/resources/ZotPediaVisual.PNG)'}/>
-        <ProjectCard />
+
+        <Link className="linkStyle" to='https://www.ochillel.org/' target="_blank">
+          <ProjectCard url={'url(https://raw.githubusercontent.com/DaniBoie/TS-React-Portfolio/main/src/resources/OC-Hillel-logo.png-OC%2BHillel%2Blogo.png)'}/>
+        </Link>
+        
+        <Link className="linkStyle" to='https://www.figma.com/proto/6zGW25nzlwmHFHtBhS6kF9?node-id=251-47&mode=design&t=RlV5BqQWL5A9WDcm-6' target='_blank'>
+          <ProjectCard url={'url(https://raw.githubusercontent.com/DaniBoie/TS-React-Portfolio/main/src/resources/ZotPediaVisual.PNG)'}/>
+        </Link>
+
+        <Link className="linkStyle" to='https://www.figma.com/proto/HH5tdtQbHDJGyEYtMUMTVb?node-id=0-1&mode=design&t=g8hz04v9nsTFieNi-6' target='_blank'>
+          <ProjectCard url={'url(https://raw.githubusercontent.com/DaniBoie/TS-React-Portfolio/main/src/resources/ZotPediaVisual.PNG)'}/>
+        </Link>
+        
       </div>
       
     </>
